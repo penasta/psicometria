f48f30d8717f27123aef292d91e926ff0ac17161
Bruno Gondim Toledo <toledis@gmail.com>
2023-09-25 09:58:37 -0300

    Initial commit

---
+++ arquivos/README.txt
@@ -0,0 +1,3 @@
+GERENTES: Insira nessa pasta todos os arquivos compartilhados com cliente, alocados, etc. Contrato, proposta, arquivos enviados pela cliente, tudo que possa ser passível de consulta em algum momento. PREFERENCIALMENTE, utilize o arquivo alocacao.txt para inserir as análises a serem executadas conforme modelo, para consulta dos alocados quando necessário. NOMEIE os arquivos com nomes que auxiliem a consulta.
+
+ALOCADOS: Procurem nessa pasta arquivos pertinentes para sua análise antes de pedir ajuda, evitando assim possíveis redundâncias de arquivos já compartilhados.
---
+++ rdocs/source/packages.R
@@ -0,0 +1,62 @@
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(
+  tidyverse, data.table,
+  readxl, readr, ggcorrplot, cowplot,
+  RColorBrewer, scales, nortest, xlsx
+)
+windowsFonts(Arial=windowsFont("sans"))
+
+options(scipen=999)
+
+# Definindo paleta de cores da Estat
+cores_estat <- c(
+  "#A11D21", "#003366", "#CC9900", "#663333", "#FF6600",
+  "#CC9966", "#999966", "#006606", "#008091", "#041835",
+  "#666666")
+
+theme_estat <- function(...) {
+  theme <- ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.title.y = ggplot2::element_text(colour = "black", size = 10),
+      axis.title.x = ggplot2::element_text(colour = "black", size = 10),
+      axis.text = ggplot2::element_text(colour = "black", size = 10),
+      panel.border = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour = "black"),
+      text = element_text(family = "sans", size = 12),
+      legend.position = "top",
+      ...
+    )
+  return(
+    list(
+      theme,
+      scale_fill_manual(values = cores_estat),
+      scale_colour_manual(values = cores_estat),
+      scale_y_continuous(
+        labels = scales::number_format(decimal.mark = ',',
+                                       #                                       accuracy = 0.01,
+                                       big.mark = "."))
+    )
+  )
+}
+
+# Definindo função que retorna frequências relativas de um vetor
+percent <- function(absolute, digits = 2) {
+  return(round(100 * absolute / sum(absolute), digits))
+}
+
+# Definindo função que retorna banco de dados com frequências
+# relativas e absolutas de uma variável categórica
+vector_frequencies <- function(vector) {
+  frequency <- vector %>%
+    table() %>%
+    as_tibble() %>%
+    mutate(
+      rel = n %>%
+        percent() %>%
+        paste("%", sep = "")
+    )
+  colnames(frequency) <- c("groups", "absolute", "relative")
+  return(frequency)
+}
+
